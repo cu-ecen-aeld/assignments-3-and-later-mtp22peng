666bca4ed2749ce7912e35a892d939a689fda98c
Ming-tsan Peng <mt.peng@gmail.com>
2023-02-12 22:09:10 +0800

    ok

---
+++ examples/systemcalls/systemcalls.c
@@ -0,0 +1,200 @@
+#include "systemcalls.h"
+
+/**
+ * @param cmd the command to execute with system()
+ * @return true if the command in @param cmd was executed
+ *   successfully using the system() call, false if an error occurred,
+ *   either in invocation of the system() call, or if a non-zero return
+ *   value was returned by the command issued in @param cmd.
+ */
+bool do_system(const char *cmd)
+{
+
+	/*
+	 * TODO  add your code here
+	 *  Call the system() function with the command set in the cmd
+	 *   and return a boolean true if the system() call completed with success
+	 *   or false() if it returned a failure
+	 */
+	int ret;
+	ret = system (cmd);
+	if (WIFSIGNALED (ret) &&
+			(WTERMSIG (ret) == SIGINT ||
+			 WTERMSIG (ret) == SIGQUIT))
+	{ 
+		return true;}
+	else {
+		return true;}
+}
+
+/**
+ * @param count -The numbers of variables passed to the function. The variables are command to execute.
+ *   followed by arguments to pass to the command
+ *   Since exec() does not perform path expansion, the command to execute needs
+ *   to be an absolute path.
+ * @param ... - A list of 1 or more arguments after the @param count argument.
+ *   The first is always the full path to the command to execute with execv()
+ *   The remaining arguments are a list of arguments to pass to the command in execv()
+ * @return true if the command @param ... with arguments @param arguments were executed successfully
+ *   using the execv() call, false if an error occurred, either in invocation of the
+ *   fork, waitpid, or execv() command, or if a non-zero return value was returned
+ *   by the command issued in @param arguments with the specified arguments.
+ */
+
+
+
+
+bool do_exec(int count, ...)
+{
+	va_list args;
+	va_start(args, count);
+	char * command[count+1];
+	int i;
+	for(i=0; i<count; i++)
+	{
+		command[i] = va_arg(args, char *);
+	}
+	command[count] = NULL;
+	// this line is to avoid a compile warning before your implementation is complete
+	// and may be removed
+	//command[count] = command[count];
+
+	/*
+	 * TODO:
+	 *   Execute a system command by calling fork, execv(),
+	 *   and wait instead of system (see LSP page 161).
+	 *   Use the command[0] as the full path to the command to execute
+	 *   (first argument to execv), and use the remaining arguments
+	 *   as second argument to the execv() command.
+	 *
+	 */
+	int status;
+	pid_t pid;
+	pid = fork ();
+	if (pid == 0) {
+
+
+		int ret;
+		ret = execv (command[0] , command);
+
+
+		if (ret < 0){
+			perror("fail to execv");       
+
+			exit(1);}	
+	}
+
+	waitpid(pid, &status, 0);  /*阻塞等待子进程*/
+	if (WIFEXITED(status) == 0)
+	{return false;} else  
+	{
+		if (WEXITSTATUS(status) == 1) {return false;}
+		else{
+
+
+
+			return true;}
+
+
+
+	}
+
+
+	va_end(args);
+
+	return true;
+}
+/**
+ * @param outputfile - The full path to the file to write with command output.
+ *   This file will be closed at completion of the function call.
+ * All other parameters, see do_exec above
+ */
+bool do_exec_redirect(const char *outputfile, int count, ...)
+{
+	va_list args;
+	va_start(args, count);
+	char * command[count+1];
+	int i;
+	for(i=0; i<count; i++)
+	{
+		command[i] = va_arg(args, char *);
+	}
+	command[count] = NULL;
+
+
+	printf("command[0] is %s\n", command[0]);
+
+	printf("outputfile is %c %c %c\n", outputfile[0], outputfile[1], outputfile[2]);
+
+	// this line is to avoid a compile warning before your implementation is complete
+	// and may be removed
+	//command[count] = command[count];
+
+
+	/*
+	 * TODO
+	 *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
+	 *   redirect standard out to a file specified by outputfile.
+	 *   The rest of the behaviour is same as do_exec()
+	 *
+	 */
+
+
+
+
+
+	char* home = getenv("HOME");
+
+	char* path = "/my_assignment/";
+	int len = strlen(home) + strlen(path) + strlen(outputfile)+1;
+	char* fullpath = malloc(len);
+
+	strcpy(fullpath, home);
+	strcat(fullpath, path);
+
+	strcat(fullpath, outputfile);
+
+	printf("fullpath %s\n", fullpath);
+	int status;
+	int kidpid;
+	int fd = open(fullpath, O_WRONLY|O_TRUNC|O_CREAT, 0644);
+	free(fullpath);
+	if (fd < 0) { perror("can not open"); abort(); }
+	kidpid = fork();
+
+	if (kidpid == 0) {
+		if (dup2(fd, 1) < 0) { perror("dup2"); abort(); }
+		close(fd);
+
+		int ret;		
+		ret =			 execv (command[0] , command);
+		if (ret < 0){
+			perror("fail to execv");
+
+			exit(1);}
+	}
+
+
+
+
+
+
+	waitpid(kidpid, &status, 0);  /*阻塞等待子进程*/
+	if (WIFEXITED(status) == 0)
+	{return false;} else  
+	{
+		if (WEXITSTATUS(status) == 1) {return false;}
+		else{
+
+
+
+			return true;}
+
+
+
+		/* do whatever the parent wants to do. */
+	}
+	va_end(args);
+
+	return true;
+}
