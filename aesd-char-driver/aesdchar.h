d38cbb2fd5d3b38bcecf0c4ebbe96eaff543f892
mtp22 <mt.peng@gmail.com>
2023-10-03 16:22:09 +0800

    ok

--- aesd-char-driver/aesdchar.h
+++ aesd-char-driver/aesdchar.h
@@ -8,6 +8,9 @@
 #ifndef AESD_CHAR_DRIVER_AESDCHAR_H_
 #define AESD_CHAR_DRIVER_AESDCHAR_H_
 
+#include "linux/cdev.h"
+#include "linux/mutex.h"
+#include "aesd_ioctl.h"
 #define AESD_DEBUG 1  //Remove comment on this line to enable debug
 
 #undef PDEBUG             /* undef it, just in case */
@@ -22,24 +25,17 @@
 #else
 #  define PDEBUG(fmt, args...) /* not debugging: nothing */
 #endif
-
-
-
 #include "aesd-circular-buffer.h"
-
 struct aesd_dev
 {
     /**
      * TODO: Add structure(s) and locks needed to complete assignment requirements
      */
-   
-       
-struct aesd_circular_buffer buffer;
+
+    struct aesd_circular_buffer buffer;
     struct aesd_buffer_entry working_entry;
     struct mutex mut;
-
-
-	struct cdev cdev;     /* Char device structure      */
+    struct cdev cdev;     /* Char device structure      */
 };
 
 
